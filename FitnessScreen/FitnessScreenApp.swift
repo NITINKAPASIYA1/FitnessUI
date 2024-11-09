47eaae7e146988f9ba7452768884178cc412034c
Nitin <kumarnitin5814@gmail.com>
2024-11-09 18:56:24 +0530

    Initial Commit

---
+++ FitnessScreen/FitnessScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessScreenApp.swift
+//  FitnessScreen
+//
+//  Created by Nitin ‘s on 09/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FitnessScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
